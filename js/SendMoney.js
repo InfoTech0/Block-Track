92f94efc05014282dc5eab5dc98777fe9498f286
MoDedSec <mo2a.abd2lhamed@gmail.com>
2020-08-15 16:33:21 +0200

    Edit Send Money Page

--- js/SendMoney.js
+++ js/SendMoney.js
@@ -8,7 +8,7 @@ function makeid(length) {
       return result;
       }
       var Mid=(makeid(2));
-
+      console.log(Mid);
       function createasset() {
         var Pid = document.getElementById("project").value;
         var mo = document.getElementById("money").value;
@@ -22,8 +22,11 @@ function makeid(length) {
 
         xhr.onreadystatechange = function() { // Call a function when the state changes.
             if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-              console.log("First Success");
-
+              alert("MoneyID Is: "+Mid);
+              SendMoney();
+            }
+            else if (this.readyState === XMLHttpRequest.DONE && this.status === 500) {
+              alert("Check Project ID and Try Again");
             }
         }
         var x=
@@ -52,7 +55,10 @@ function makeid(length) {
 
              xhr.onreadystatechange = function() { // Call a function when the state changes.
                  if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-                     alert("Send Money Success");
+                     alert("Transaction Success");
+                 }
+                 else if (this.readyState === XMLHttpRequest.DONE && this.status === 500) {
+                   SendMoney();
                  }
              }
              var x=
